61d37575ab7200414cb1893db59123290bbb6fe6
Evandro <evandrow1010@gmail.com>
2025-05-26 23:25:09 -0300

    NexMoney - Criação de arquivo base de importação de ofx

--- Front/src/pages/bank-statement/index.jsx
+++ Front/src/pages/bank-statement/index.jsx
@@ -1,3 +1,23 @@
+//Importação de arquivo ofx
+import { FileUpload } from "./components/file_upload";
+import { Typography, Container, Box } from "@mui/material";
+
 export const BankStatementPage = () => {
-  return <div>BankStatementPage</div>;
+  return (
+    <div className="flex gap-4 flex-col bg-background bg-[#1B2232] height-screen  h-screen w-full overflow-hidden">
+      <Container maxWidth="md" sx={{ py: 4 }}>
+        <Typography variant="h4" gutterBottom color="text.primary">
+          Importar Extrato Bancário
+        </Typography>
+
+        <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+          Carregue seu extrato bancário no formato OFX para importar transações.
+        </Typography>
+
+        <Box sx={{ mt: 4 }}>
+          <FileUpload />
+        </Box>
+      </Container>
+    </div>
+  );
 };
