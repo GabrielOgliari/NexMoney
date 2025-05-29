526d486359314a4d203931f4a36dc0ef8e0455d2
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2025-05-29 16:38:43 -0300

    fix: ajuste de componente generic form

--- Front/src/components/ui/GenericFormModal.jsx
+++ Front/src/components/ui/GenericFormModal.jsx
@@ -1,6 +1,14 @@
-// GenericFormModal.jsx
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  MenuItem,
+} from "@mui/material";
 import { Formik, Form, Field } from "formik";
+import { useEffect, useRef } from "react";
 
 export function GenericFormModal({
   open,
@@ -10,54 +18,105 @@ export function GenericFormModal({
   fields,
   validationSchema,
   title = "Formulário",
-  submitLabel = "Salvar"
+  submitLabel = "Salvar",
+  onChange,
 }) {
+  // Ref para armazenar o último valor enviado
+  const lastValues = useRef(initialValues);
+
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
       <DialogTitle>{title}</DialogTitle>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        enableReinitialize
         onSubmit={onSubmit}
+        enableReinitialize
       >
-        {({ errors, touched, handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
-            <DialogContent>
-              <div className="flex flex-col gap-4">
-                {fields.map((field) => (
-                  <Field
-                    key={field.name}
-                    name={field.name}
-                    as={TextField}
-                    label={field.label}
-                    type={field.type || "text"}
-                    select={field.type === "select"}
-                    fullWidth
-                    error={!!errors[field.name] && touched[field.name]}
-                    helperText={touched[field.name] && errors[field.name]}
-                    {...(field.options ? { SelectProps: { native: true } } : {})}
-                  >
-                    {field.options &&
-                      field.options.map((opt) => (
-                        <option key={opt.value} value={opt.value}>
-                          {opt.label}
-                        </option>
-                      ))}
-                  </Field>
-                ))}
-              </div>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={onClose} color="inherit">
-                Cancelar
-              </Button>
-              <Button type="submit" color="primary" variant="contained">
-                {submitLabel}
-              </Button>
-            </DialogActions>
-          </Form>
-        )}
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+        }) => {
+          // useEffect fora do retorno JSX!
+          useEffect(() => {
+            if (
+              typeof onChange === "function" &&
+              JSON.stringify(values) !== JSON.stringify(lastValues.current)
+            ) {
+              onChange(values);
+              lastValues.current = values;
+            }
+          }, [values, onChange]);
+
+          return (
+            <Form onSubmit={handleSubmit}>
+              <DialogContent>
+                <div className="flex flex-col gap-4">
+                  {fields.map((field) =>
+                    field.type === "select" ? (
+                      <Field name={field.name} key={field.name}>
+                        {({ field: formikField }) => (
+                          <TextField
+                            {...formikField}
+                            label={field.label}
+                            select
+                            fullWidth
+                            error={!!errors[field.name] && touched[field.name]}
+                            helperText={
+                              touched[field.name] && errors[field.name]
+                            }
+                            value={values[field.name]}
+                            onChange={(e) => {
+                              setFieldValue(field.name, e.target.value);
+                            }}
+                          >
+                            {field.options &&
+                              field.options.map((opt) => (
+                                <MenuItem key={opt.value} value={opt.value}>
+                                  {opt.label}
+                                </MenuItem>
+                              ))}
+                          </TextField>
+                        )}
+                      </Field>
+                    ) : (
+                      <Field name={field.name} key={field.name}>
+                        {({ field: formikField }) => (
+                          <TextField
+                            {...formikField}
+                            label={field.label}
+                            type={field.type || "text"}
+                            fullWidth
+                            error={!!errors[field.name] && touched[field.name]}
+                            helperText={
+                              touched[field.name] && errors[field.name]
+                            }
+                            value={values[field.name]}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                          />
+                        )}
+                      </Field>
+                    )
+                  )}
+                </div>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={onClose} color="inherit">
+                  Cancelar
+                </Button>
+                <Button type="submit" color="primary" variant="contained">
+                  {submitLabel}
+                </Button>
+              </DialogActions>
+            </Form>
+          );
+        }}
       </Formik>
     </Dialog>
   );
